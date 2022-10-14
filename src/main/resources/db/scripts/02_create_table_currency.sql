1ffadb14c5f07381f69d5ad5fd54ddfb5c748737
Clauribi <claudiarivasbielsa@gmail.com>
2022-10-14 19:59:12 +0200

    finish

---
+++ src/main/resources/db/scripts/02_create_table_currency.sql
@@ -0,0 +1,6 @@
+create table currency(
+	fecha varchar(10) PRIMARY KEY,
+	euro double NOT NULL,
+	dollar double,
+	pound double
+);
---
+++ src/main/resources/db/scripts/03_insert_values_currency.sql
@@ -0,0 +1,4 @@
+insert into currency values("07-10-2022", 1,1,0.62);
+insert into currency values("08-10-2022", 1,0.90,0.80);
+insert into currency values("09-10-2022", 1,0.99,0.80);
+insert into currency values("07-10-2022", 1,0.98,0.79);
